8250726c5619259af2b3ff8c8cc553c1d1c378c8
eshan1jaffar <80078879+eshan1jaffar@users.noreply.github.com>
2024-10-27 00:43:16 -0400

    created button functionalities

--- app/src/main/java/com/example/braillebuddy/MainActivity.java
+++ app/src/main/java/com/example/braillebuddy/MainActivity.java
@@ -117,12 +117,28 @@ import java.util.ArrayList;
                         Log.d("SWIPE", "right swipe");
                         anotherActivity = new Intent(this, BrailleMappingActivity.class);
                         startActivity(anotherActivity);
+                        try {
+                            controller.playPattern("K");
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+
                     } else if (deltaX < MIN_DISTANCE*-1) {
                         Log.d("SWIPE", "left swipe");
                         anotherActivity = new Intent(this, MainActivity.class);
                         startActivity(anotherActivity);
+                        try {
+                            controller.playPattern("C");
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
                     } else if (deltaY > MIN_DISTANCE) {
                         Log.d("SWIPE", "down swipe");
+                        try {
+                            controller.playPattern(matches.get(0));
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
                     }
             }
             return super.onTouchEvent(event);
